49b7c4ef9b20f3131a979c470227f233fc3ca623
asvo <asvo@ucsd.edu>
2025-05-05 21:10:27 -0700

    Finish Expose implementation

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,44 @@
 // expose.js
-
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  const soundSelect = document.getElementById("horn-select");
+  const volumeSelect = document.getElementById("volume");
+  const playButton = document.querySelector("button");
+  const audio = document.querySelector("audio");
+  const soundImage = document.querySelector("img");
+  const volumeImage = document.querySelector("#volume-controls img");
+  const jsConfetti = new JSConfetti;
+
+  soundSelect.addEventListener("change", 
+    (event) => {
+      audio.src = `assets/audio/${event.target.value}.mp3`;
+      soundImage.src = `assets/images/${event.target.value}.svg`;
+    });
+
+  playButton.addEventListener("click",
+    (event) => { 
+      audio.play();
+
+      const dropdownValue = soundSelect.value;
+      if (dropdownValue === "party-horn") {
+        jsConfetti.addConfetti();
+      }
+    });
+
+  volumeSelect.addEventListener("change",
+    (event) => { 
+      const volumeVal = Number(event.target.value);
+      audio.volume = volumeVal / 100;
+
+      if (volumeVal === 0) {
+        volumeImage.src = `assets/icons/volume-level-0.svg`;
+      } else if (volumeVal >= 1 && volumeVal < 33) {
+        volumeImage.src = `assets/icons/volume-level-1.svg`;
+      } else if (volumeVal >= 33 && volumeVal < 67) {
+        volumeImage.src = `assets/icons/volume-level-2.svg`;
+      } else {
+        volumeImage.src = `assets/icons/volume-level-3.svg`;
+      }
+    });
 }
